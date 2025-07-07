99990a3626671a4dcf1d57f8b9c3131cd61d5ed9
Kamehameha010 <mizukaze_010@hotmail.com>
2025-07-07 00:59:45 -0600

    added page router

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,14 +1,86 @@
-import { AuthProvider } from './context/auth-context'
-import { Index } from './routes/tasks/index'
+import { createRootRouteWithContext, createRoute, createRouter, Outlet, redirect, RouterProvider } from '@tanstack/react-router';
+import { AuthProvider } from './context/auth-context';
+import { Index as AuthIndex } from './routes/access/index';
+import { Index as TaskIndex } from './routes/tasks/index';
+
+interface AuthContext {
+  auth: {
+    isAuthenticated: boolean
+  }
+}
+
+const rootRoute = createRootRouteWithContext<AuthContext>()({
+  component: () => <Outlet />
+});
+
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  component: () => <Outlet />,
+  beforeLoad: ({ context }) => {
+    if (!context.auth.isAuthenticated) {
+      throw redirect({
+        to: "/access",
+        replace: true
+      })
+
+    }
+
+    throw redirect({
+      to: "/tasks",
+      replace: true
+    })
+
+  }
+});
+
+const authRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/access',
+  component: AuthIndex,
+  beforeLoad: ({ context }) => {
+    if (context.auth.isAuthenticated) {
+      throw redirect({
+        to: "/tasks",
+        replace: true
+      })
+    }
+
+  }
+
+});
+
+const tasksRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/tasks',
+  component: TaskIndex,
+  beforeLoad: ({ context }) => {
+    if (!context.auth.isAuthenticated) {
+      throw redirect({
+        to: "/access",
+        replace: true
+      })
+    }
+
+  }
+});
 
 
+const routeTree = rootRoute.addChildren([indexRoute, tasksRoute, authRoute]);
+
+const router = createRouter({
+  routeTree,
+  context: {
+    auth: {
+      isAuthenticated: false
+    }
+  }
+});
 
 export function App() {
   return (
-    <>
-      <AuthProvider>
-        <Index />
-      </AuthProvider>
-    </>
-  )
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  );
 }
